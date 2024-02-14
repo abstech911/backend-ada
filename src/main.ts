4154d24adf02cc3114c3d93fd6c66503d4d1fbae
Abubakar Sani <abubakarsani@ravenandmacaw.com>
2024-02-14 19:48:40 +0100

    Added Cors part 3

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     dotenv.config();
     app.enableCors({
-        origin: ['https://YOUR-APP-NAME.vercel.app', 'http://localhost:3000'],
+        origin: ['https://backend-ada.vercel.app/', 'http://localhost:3000'],
         methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
         credentials: true,
-        allowedHeaders:
-            'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+        allowedHeaders: ['Content-Type', 'Authorization'],
     });
+
     app.useGlobalPipes(new ValidationPipe(
         {
             whitelist: true,
