7f2845a8c6219f5833e0a510293a28d1ac23b09d
Abubakar Sani <abubakarsani@ravenandmacaw.com>
2024-02-14 20:56:16 +0100

    Fixed CORS

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,21 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     dotenv.config();
+    // app.enableCors({
+    //     origin: '*', // Allow requests from this origin
+    //     methods: ['GET', 'POST'], // Allow only GET and POST requests
+    //     allowedHeaders: ['Content-Type', 'Authorization'], //
+    // });
+    const whitelist = ['http://localhost:3001', 'api.example.com'];
     app.enableCors({
-        origin: '*', // Allow requests from this origin
-        methods: ['GET', 'POST'], // Allow only GET and POST requests
-        allowedHeaders: ['Content-Type', 'Authorization'], //
+        origin: function (origin, callback) {
+            if (!origin || whitelist.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        },
     });
-
     app.useGlobalPipes(new ValidationPipe(
         {
             whitelist: true,
