ce6ce10bd15161b8c20a7a2f616f02fec2a0b0ae
Abubakar Sani <abubakarsani@ravenandmacaw.com>
2024-02-14 21:48:13 +0100

    CORS JUST CPU version 9

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,16 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import * as dotenv from 'dotenv';
-import * as cors from 'cors';
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     dotenv.config();
 
-    app.use(
-        cors({
-            origin: 'http://localhost:3001', // Allow requests from this origin
-            methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], // Allow specified methods
-            allowedHeaders: ['Content-Type', 'Authorization'], // Allow specified headers
-        })
-    );
+    app.enableCors({
+        origin: '*', // Allow requests from all origins
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specified methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Allow specified headers
+    });
     app.useGlobalPipes(new ValidationPipe(
         {
             whitelist: true,
